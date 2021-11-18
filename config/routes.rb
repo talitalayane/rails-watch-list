8f5b6930bfceff91eb6695ed0890011501866322
talitalayane <talitalayane@hotmail.com>
2021-11-18 17:02:06 -0300

    feito rotas, parte do controller e da view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+
+  resources :lists, except: %i[edit update destroy] do
+    resources :bookmarks, only: %i[ new create destroy], shallow: true
+
+  end
 end
