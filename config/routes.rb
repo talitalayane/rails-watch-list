616e3de4a3c0e8d3c6b7e05ac2ca545aa6f40e21
talitalayane <talitalayane@hotmail.com>
2021-11-18 19:53:21 -0300

    falta show do list

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-
-  resources :lists, except: %i[edit update destroy] do
-    resources :bookmarks, only: %i[ new create destroy], shallow: true
-
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
